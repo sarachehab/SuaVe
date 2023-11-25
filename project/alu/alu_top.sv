ae3d27c1b205a60536a1c763787a3c342123d87c
root <root@LuJu>
2023-11-25 03:51:16 +0000

    renamed variables from eg alusrc1 to src1

--- project/alu/alu_top.sv
+++ project/alu/alu_top.sv
@@ -1,25 +1,25 @@
 module alu_top#(
     parameter DATA_WIDTH = 32
 )(
-    input  logic [DATA_WIDTH-1:0] alusrc1, alusrc2,
+    input  logic [DATA_WIDTH-1:0] src1, src2,
     input  logic [2:0] aluctrl,
     output logic [DATA_WIDTH-1:0] aluresult,
     output logic eq
 );
 
 always_comb begin 
-    eq = alusrc1 == alusrc2;
+    eq = src1 == src2;
     case (aluctrl)
         // add
-        000: aluresult = alusrc1 + alusrc2;
+        000: aluresult = src1 + src2;
         // subtract
-        001: aluresult = alusrc1 - alusrc2;
+        001: aluresult = src1 - src2;
         // and
-        010: aluresult = alusrc1 & alusrc2;
+        010: aluresult = src1 & src2;
         // or
-        011: aluresult =alusrc1 | alusrc2;
+        011: aluresult = src1 | src2;
         // SLT
-        101: aluresult = alusrc2 - alusrc1;
+        101: aluresult = src2 - src1;
         default: aluresult = DATA_WIDTH'b0;
     endcase 
 end
