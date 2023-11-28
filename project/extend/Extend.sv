a8b685d844b31c1eba408e7b4016b28772714e7b
Nr722 <nr722@ic.ac.uk>
2023-11-28 14:59:39 +0000

    changed names of the signals to match standard

--- project/extend/Extend.sv
+++ project/extend/Extend.sv
@@ -1,21 +1,29 @@
-module Extend # (
-    parameter  DATA_WIDTH = 32
+module signExtend # (
+    parameter DATA_WIDTH = 32
 )(
-    input  logic [DATA_WIDTH-1:7] Instr, //Instruction ommiting OpCode (last 7 bits)
-    input  logic [1:0] ImmSrc,
-    output logic [DATA_WIDTH-1:0] ImmExt //output imm extended into 32 bits
+    //input signals
+    input  logic [INSTRUCT_WIDTH-1:7] instr_i, //instruction
+    input  logic [1:0] imm_src_i,
+    input logic [DATA_WIDTH-1:0] pc_i,
+    //output
+    output logic [DATA_WIDTH-1:0] imm_ext_o, //output imm extended into 32 bits
+    output logic [DATA_WIDTH-1:0] pc_target_o,
 );
 
 always_comb
-    case(ImmSrc)
+    case(immsrc)
+
         //type I
-        2'b00: ImmExt = {{20{Instr[31]}}, Instr[31:20]};
+        2'b00: imm_ext_o = {{20{instr_i[31]}}, instr_i[31:20]};
         // type S
-        2'b01: ImmExt = {{20{Instr[31]}}, Instr[31:25], Instr[11:7]};
+        2'b01: imm_ext_o = {{20{instr_i[31]}}, instr_i[31:25], instr_i[11:7]};
         //type B
-        2'b10: ImmExt = {{20{Instr[31]}}, Instr[7], Instr[30:25], Instr[11:8], 1'b0}; 
+        2'b10: imm_ext_o = {{20{instr_i[31]}}, instr_i[7], instr_i[30:25], instr_i[11:8], 1'b0}; 
         //type Jal
-        2'b11: ImmExt = {{12{Instr[31]}}, Instr[19:12], Instr[20], Instr[30:21], 1'b0};
-        default: ImmExt = {DATA_WIDTH{1b'0}};
+        2'b11: imm_ext_o = {{12{instr_i[31]}}, instr_i[19:12], instr_i[20], instr_i[30:21], 1'b0};
+        default: imm_ext_o = 32'b0;
+
     endcase
+    assign pc_target_o = imm_ext_o + pc_i;
+
 endmodule
