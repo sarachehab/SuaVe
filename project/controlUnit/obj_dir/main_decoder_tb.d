5556d24a493c099c1409b024b44bbd6dcee8d661
Sara Chehab <sara@chehab.co>
2023-11-29 21:03:35 +0000

    wrote testbench and tested main main_decoder
    - fixed minor typos
    - TODO: check ALUsrc for JAL
    - TODO: add UpperImmediate U instructions

---
+++ project/controlUnit/obj_dir/main_decoder_tb.d
@@ -0,0 +1,10 @@
+main_decoder_tb.o: ../main_decoder_tb.cpp \
+  /opt/homebrew/Cellar/verilator/5.016/share/verilator/include/verilated.h \
+  /opt/homebrew/Cellar/verilator/5.016/share/verilator/include/verilatedos.h \
+  /opt/homebrew/Cellar/verilator/5.016/share/verilator/include/verilated_config.h \
+  /opt/homebrew/Cellar/verilator/5.016/share/verilator/include/verilated_types.h \
+  /opt/homebrew/Cellar/verilator/5.016/share/verilator/include/verilated_funcs.h \
+  /opt/homebrew/Cellar/verilator/5.016/share/verilator/include/verilated_vcd_c.h \
+  /opt/homebrew/Cellar/verilator/5.016/share/verilator/include/verilated_trace.h \
+  /opt/homebrew/Cellar/verilator/5.016/share/verilator/include/verilated_trace_defs.h \
+  Vmain_decoder.h
