f78a717c022bc1787024750c7fa1ac9433a2648e
Nr722 <nr722@ic.ac.uk>
2023-11-30 10:30:15 +0000

    done to add U type instr

--- project/extend/Extend.sv
+++ project/extend/Extend.sv
@@ -3,7 +3,7 @@ module signExtend # (
 )(
     //input signals
     input  logic [INSTRUCT_WIDTH-1:7] instr_i, //instruction
-    input  logic [1:0] imm_src_i,
+    input  logic [2:0] imm_src_i,
     input logic [DATA_WIDTH-1:0] pc_i,
     //output
     output logic [DATA_WIDTH-1:0] imm_ext_o, //output imm extended into 32 bits
@@ -14,13 +14,16 @@ always_comb
     case(immsrc)
 
         //type I
-        2'b00: imm_ext_o = {{20{instr_i[31]}}, instr_i[31:20]};
+        3'b000: imm_ext_o = {{20{instr_i[31]}}, instr_i[31:20]};
         // type S
-        2'b01: imm_ext_o = {{20{instr_i[31]}}, instr_i[31:25], instr_i[11:7]};
+        3'b001: imm_ext_o = {{20{instr_i[31]}}, instr_i[31:25], instr_i[11:7]};
         //type B
-        2'b10: imm_ext_o = {{20{instr_i[31]}}, instr_i[7], instr_i[30:25], instr_i[11:8], 1'b0}; 
+        3'b010: imm_ext_o = {{20{instr_i[31]}}, instr_i[7], instr_i[30:25], instr_i[11:8], 1'b0}; 
         //type Jal
-        2'b11: imm_ext_o = {{12{instr_i[31]}}, instr_i[19:12], instr_i[20], instr_i[30:21], 1'b0};
+        3'b011: imm_ext_o = {{12{instr_i[31]}}, instr_i[19:12], instr_i[20], instr_i[30:21], 1'b0};
+        
+        //type U
+        3'b100: imm_ext_o = {12{instr_i[31]}, instr_i[31:12]}
         default: imm_ext_o = 32'b0;
 
     endcase
