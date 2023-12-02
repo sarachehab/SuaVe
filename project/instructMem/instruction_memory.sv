c55b7ff9c58d30017bfca594a0dd2c2d97f5b822
Sara Chehab <sara@chehab.co>
2023-12-02 02:40:02 +0000

    wrote testbench for instruction instruction_memory
    - corrected typos in instruction_memory.sv
    - added testbench
    
    TODO: Find and fix source of segmentation error

--- project/instructMem/instruction_memory.sv
+++ project/instructMem/instruction_memory.sv
@@ -1,20 +1,26 @@
 module instruction_memory #(
-    parameter   NB_ADDR = 12,
+    parameter   ADDR_EXP = 12,
                 BYTE_SIZE = 8,
-                INSTR_WIDTH = 32,
-                FIRST_INSTRUCTION_ADDR = 32'hBFC00000,
-                LAST_INSTRUCTION_ADDR = 32'hBFC00FFF
+                DATA_WIDTH = 32,
+                FIRST_INSTR_ADDR = 32'hBFC00000,
+                LAST_INSTR_ADDR = 32'hBFC00FFF
 )(
-    input logic [A_WIDTH-1:0] address_i,
-    output logic [INSTR_WIDTH-1:0] instruction_o
+    input logic [DATA_WIDTH-1:0] addr_i,
+    output logic [DATA_WIDTH-1:0] instr_o
 );
 
-logic [BYTE_SIZE-1:0] mem [(2**NB_ADDR)-1:0];
+logic [BYTE_SIZE-1:0] rom [2**ADDR_EXP-1:0];
 
 initial begin
-    $readmemh("instructions.mem", mem, );
+
+    $display("Loading rom.");
+    $readmemh("instructions.mem", rom);
+    $display("Program written to ROM successfully");
+
 end;
 
-assign instruction_o = {mem[(address_i & 32'hFFFFFFFC)+32'd3], mem[(address_i & 32'hFFFFFFFC)+32'd2], mem[(address_i & 32'hFFFFFFFC)+32'd1], mem[(address_i & 32'hFFFFFFFC)]}
+logic [DATA_WIDTH-1:0] normalized_address = (addr_i - FIRST_INSTR_ADDR) & 32'hFFFFFFFC;
+
+assign instr_o = {rom[normalized_address+32'd3], rom[normalized_address+32'd2], rom[normalized_address+32'd1], rom[normalized_address]};
 
 endmodule
---
+++ project/instructMem/obj_dir/Vinstruction_memory__Trace__0__Slow.cpp
@@ -0,0 +1,77 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Tracing implementation internals
+#include "verilated_vcd_c.h"
+#include "Vinstruction_memory__Syms.h"
+
+
+VL_ATTR_COLD void Vinstruction_memory___024root__trace_init_sub__TOP__0(Vinstruction_memory___024root* vlSelf, VerilatedVcd* tracep) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vinstruction_memory__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vinstruction_memory___024root__trace_init_sub__TOP__0\n"); );
+    // Init
+    const int c = vlSymsp->__Vm_baseCode;
+    // Body
+    tracep->declBus(c+1,"addr_i", false,-1, 31,0);
+    tracep->declBus(c+2,"instr_o", false,-1, 31,0);
+    tracep->pushNamePrefix("instruction_memory ");
+    tracep->declBus(c+4,"ADDR_EXP", false,-1, 31,0);
+    tracep->declBus(c+5,"BYTE_SIZE", false,-1, 31,0);
+    tracep->declBus(c+6,"DATA_WIDTH", false,-1, 31,0);
+    tracep->declBus(c+7,"FIRST_INSTR_ADDR", false,-1, 31,0);
+    tracep->declBus(c+8,"LAST_INSTR_ADDR", false,-1, 31,0);
+    tracep->declBus(c+1,"addr_i", false,-1, 31,0);
+    tracep->declBus(c+2,"instr_o", false,-1, 31,0);
+    tracep->declBus(c+3,"normalized_address", false,-1, 31,0);
+    tracep->popNamePrefix(1);
+}
+
+VL_ATTR_COLD void Vinstruction_memory___024root__trace_init_top(Vinstruction_memory___024root* vlSelf, VerilatedVcd* tracep) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vinstruction_memory__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vinstruction_memory___024root__trace_init_top\n"); );
+    // Body
+    Vinstruction_memory___024root__trace_init_sub__TOP__0(vlSelf, tracep);
+}
+
+VL_ATTR_COLD void Vinstruction_memory___024root__trace_full_top_0(void* voidSelf, VerilatedVcd::Buffer* bufp);
+void Vinstruction_memory___024root__trace_chg_top_0(void* voidSelf, VerilatedVcd::Buffer* bufp);
+void Vinstruction_memory___024root__trace_cleanup(void* voidSelf, VerilatedVcd* /*unused*/);
+
+VL_ATTR_COLD void Vinstruction_memory___024root__trace_register(Vinstruction_memory___024root* vlSelf, VerilatedVcd* tracep) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vinstruction_memory__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vinstruction_memory___024root__trace_register\n"); );
+    // Body
+    tracep->addFullCb(&Vinstruction_memory___024root__trace_full_top_0, vlSelf);
+    tracep->addChgCb(&Vinstruction_memory___024root__trace_chg_top_0, vlSelf);
+    tracep->addCleanupCb(&Vinstruction_memory___024root__trace_cleanup, vlSelf);
+}
+
+VL_ATTR_COLD void Vinstruction_memory___024root__trace_full_sub_0(Vinstruction_memory___024root* vlSelf, VerilatedVcd::Buffer* bufp);
+
+VL_ATTR_COLD void Vinstruction_memory___024root__trace_full_top_0(void* voidSelf, VerilatedVcd::Buffer* bufp) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vinstruction_memory___024root__trace_full_top_0\n"); );
+    // Init
+    Vinstruction_memory___024root* const __restrict vlSelf VL_ATTR_UNUSED = static_cast<Vinstruction_memory___024root*>(voidSelf);
+    Vinstruction_memory__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    // Body
+    Vinstruction_memory___024root__trace_full_sub_0((&vlSymsp->TOP), bufp);
+}
+
+VL_ATTR_COLD void Vinstruction_memory___024root__trace_full_sub_0(Vinstruction_memory___024root* vlSelf, VerilatedVcd::Buffer* bufp) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vinstruction_memory__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vinstruction_memory___024root__trace_full_sub_0\n"); );
+    // Init
+    uint32_t* const oldp VL_ATTR_UNUSED = bufp->oldp(vlSymsp->__Vm_baseCode);
+    // Body
+    bufp->fullIData(oldp+1,(vlSelf->addr_i),32);
+    bufp->fullIData(oldp+2,(vlSelf->instr_o),32);
+    bufp->fullIData(oldp+3,((0xfffffffcU & (vlSelf->addr_i 
+                                            - (IData)(0xbfc00000U)))),32);
+    bufp->fullIData(oldp+4,(0xcU),32);
+    bufp->fullIData(oldp+5,(8U),32);
+    bufp->fullIData(oldp+6,(0x20U),32);
+    bufp->fullIData(oldp+7,(0xbfc00000U),32);
+    bufp->fullIData(oldp+8,(0xbfc00fffU),32);
+}
