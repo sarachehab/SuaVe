fff0b51870262e89c2ca8f7eb2382512a8dbc3fb
Sara Chehab <89109466+sarachehab@users.noreply.github.com>
2023-12-06 22:21:22 +0000

    Added output pc+4 to pc counter

--- project/pc/pc.sv
+++ project/pc/pc.sv
@@ -3,7 +3,7 @@ module pc # (
 )(
     input                           clk_i, jump_taken_i, rst_i, jalr_instr_i,
     input   logic   [PC_WIDTH-1:0]  imm_ext_i, jta_jalr_i,
-    output  logic   [PC_WIDTH-1:0]  pc_o
+    output  logic   [PC_WIDTH-1:0]  pc_o, pc_plus4_o
 );
 
     logic [PC_WIDTH-1:0] pc_next;
@@ -25,6 +25,7 @@ module pc # (
     end 
 
     assign pc_o = pc_next;
+    assign pc_plus4_o = pc_o + 32'd4;
 
 endmodule
 
