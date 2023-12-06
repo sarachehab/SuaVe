73f377bf27a47b5461bfe3f04cc34e08cd569bd2
Sara Chehab <sara@chehab.co>
2023-12-06 00:33:17 +0000

    added keyword "logic" to define inputs and outputs

--- project/pc/pc.sv
+++ project/pc/pc.sv
@@ -1,9 +1,9 @@
 module pc # (
     parameter PC_WIDTH = 32
 )(
-    input                 clk_i, jump_taken_i, rst_i, jalr_instr_i,
-    input  [PC_WIDTH-1:0] imm_ext_i, jta_jalr_i,
-    output [PC_WIDTH-1:0] pc_o
+    input                           clk_i, jump_taken_i, rst_i, jalr_instr_i,
+    input   logic   [PC_WIDTH-1:0]  imm_ext_i, jta_jalr_i,
+    output  logic   [PC_WIDTH-1:0]  pc_o
 );
 
     logic [PC_WIDTH-1:0] pc_next;
