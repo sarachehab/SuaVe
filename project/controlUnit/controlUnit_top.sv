7489efb981b3cf68f16fa903aef3804b699c6eee
Utsav254 <u15goel@outlook.com>
2023-12-07 09:49:54 +0000

    finalise controlunit

--- project/controlUnit/controlUnit_top.sv
+++ project/controlUnit/controlUnit_top.sv
@@ -1,46 +1,48 @@
 module controlUnit_top(
     //input signals
-    input logic zero_i,
-    input logic [6:0] op_i,
-    input logic [2:0] funct3_i,
-    input logic funct7_b5_i,
+    input logic zero,
+    input logic [6:0] op,
+    input logic [2:0] funct3,
+    input logic funct7_5,
     //output signal
-    output logic branch_o
-    output logic [1:0] jump_O
-    output logic [1:0] result_src_o,
-    output logic mem_write_o,
-    output logic alu_src_o,
-    output logic [1:0] imm_src_o,
-    output logic reg_write_o,
-    output logic [2:0] alu_control_o
-    output logic byte_address_o
+    output logic JB_instr;
+    output logic JALR_instr
+    output logic [1:0] result_src,
+    output logic mem_write,
+    output logic alu_src,
+    output logic [2:0] imm_src,
+    output logic reg_write,
+    output logic [3:0] alu_control,
+    output logic byte_address
 );
     //internal signals
     logic [1:0] alu_op;
     logic branch;
+    logic jmp;
 
 mainDecoder mainDecoder(
-    .op_i(op_i),
+    .op_i(op),
     .branch_o(branch),
-    .jump_o(jump_o),
-    .result_src_o(result_src_o),
-    .mem_write_o(mem_write_o),
-    .alu_src_o(alu_src_o),
-    .imm_src_o(imm_src_o),
-    .reg_write_o(reg_write_o),
+    .jump_o(jmp),
+    .jalr_O(JALR_instr)
+    .result_src_o(result_src),
+    .mem_write_o(mem_write),
+    .alu_src_o(alu_src),
+    .imm_src_o(imm_src),
+    .reg_write_o(reg_write),
     .alu_op_o(alu_op)
 );
 
 aluDecoder aluDecoder(
     .alu_op_i(alu_op),
-    .funct3_i(funct3_i),
-    .funct7_b5_i(funct7_b5_i),
+    .funct3_i(funct3),
+    .funct7_b5_i(funct7_5),
     .op_5_i(op_i[5]),
-    .alu_control_o(alu_control_o),
-    .byte_address_o(byte_address_o)
+    .alu_control_o(alu_control),
+    .byte_address_o(byte_address)
 );
 
-assign branch_o = (zero_i & branch)
+assign JB_instr = (zero_i & branch) | jmp;
 
 endmodule
 
