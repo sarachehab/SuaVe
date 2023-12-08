1a5bcdb81185935d04440ee3eaf44d535798df80
Utsav254 <u15goel@outlook.com>
2023-12-08 17:02:00 +0000

    make notes on cache structure

--- cache.sv
+++ cache.sv
@@ -1,4 +1,21 @@
 
+/*----message for neil------
+
+our memory range is ->                    00 01 FF FF :: 00 00 00 00
+so most significant byte is always zero   ^              ^
+so lets take same random address (in binary like) : 0000 , 1010 , 1001 , 01______00
+                                                    ^^^^   ^^^^   ||||   ^^##
+                                                        tag       block  block__byte
+so i think it might be possible to make the tag 8bit instead of 24?
+
+i am also thinkning that we should split our cache into 2 sv files with a top file: 
+  1>cache memory which 
+
+
+
+
+*/
+
 // tag == 24 bits
 // set == 4 bits
 // block offset == 2 bits
@@ -39,9 +56,27 @@ module cache (
     set <= address_i[7:4];
     block_offset <= address_i[3:2];
     byte_offset <= address_i[1:0];
-  
-//write enable to be done buy utsav
 
+/*write enable to be done buy utsav
+
+require logic to check if cache block is occupied, if it is occupied, check if proceeding block is occupied? 
+
+if entire set has been filled we need a system to fetch
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
   read_data_o = (tag==cache_tag[set][0]) ? cache_data[set][0]:
                 (tag==cache_tag[set][1]) ? cache_data[set][1]:
                 (tag==cache_tag[set][2]) ? cache_data[set][2]:
