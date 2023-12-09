d549cbcb6ce16e1cb08aa1aabda0cb78ba596c75
Sara Chehab <sara@chehab.co>
2023-12-09 00:30:37 +0000

    modified pc.sv to match schematic - added enable, active low for stalls - added support for JALR and JAL instructions TODO: write testbench and test

--- pc.sv
+++ pc.sv
@@ -1,9 +1,9 @@
 module pc # (
     parameter PC_WIDTH = 32
 )(
-    input                           clk, jump_taken_i, rst_i, en_i, jalr_instr_i,
-    input   logic   [PC_WIDTH-1:0]  imm_ext_i, jta_jalr_i,
-    output  logic   [PC_WIDTH-1:0]  pc_o, pc_plus4_o
+    input                           clk, rst_i, en_i, jalr_e_i, jb_taken_e_i,
+    input   logic   [PC_WIDTH-1:0]  jta_jalr_e_i, pc_target_e_i,
+    output  logic   [PC_WIDTH-1:0]  pc_f_o, pc_plus4_f_o
 );
 
     logic [PC_WIDTH-1:0] pc_next;
@@ -16,16 +16,16 @@ module pc # (
         if (rst_i)  pc_next <= 32'hBFC00000;
         else begin
             if (en_i)
-                case (jalr_instr_i)
-                    1'b0: pc_next <= jump_taken_i ?  (pc_next + imm_ext_i) : (pc_next + 32'd4);
-                    1'b1: pc_next <= jta_jalr_i;
+                case (jalr_e_i)
+                    1'b0: pc_next <= jb_taken_e_i ?  (pc_target_e_i) : (pc_next + 32'd4);
+                    1'b1: pc_next <= jta_jalr_e_i;
                 endcase
         end
         
     end 
 
-    assign pc_o = pc_next;
-    assign pc_plus4_o = pc_o + 32'd4;
+    assign pc_f_o = pc_next;
+    assign pc_plus4_f_o = pc_f_o + 32'd4;
 
 endmodule
 
