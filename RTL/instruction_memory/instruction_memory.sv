6f943a7ff841b05931256c90f58ef241f779b7cb
Sara Chehab <sara@chehab.co>
2023-12-13 15:15:38 +0000

    Wrote exe.sh file and reorganised repo - modified instruction and data memory to account for changes

--- RTL/instruction_memory/instruction_memory.sv
+++ RTL/instruction_memory/instruction_memory.sv
@@ -13,7 +13,7 @@ logic [BYTE_SIZE-1:0] rom [LAST_INSTR_ADDR:FIRST_INSTR_ADDR];
 initial begin
 
     $display("Loading rom.");
-    $readmemh("pdf.hex", rom);
+    $readmemh("instructions.hex", rom);
     $display("Program written to ROM successfully");
 
 end;
